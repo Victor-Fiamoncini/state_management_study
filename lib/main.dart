956a3e7cc89b82ee5a2f0d5fea6ae9b5f61febaf
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-04-10 04:32:16 -0300

    feat: add redux dispatcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:state_management_study/redux/app_store.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'State Management',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Text('Flutter Demo Home Page'),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('State Management')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button many times:'),
+            AnimatedBuilder(
+              animation: appStore,
+              builder: (_, __) {
+                return Text(
+                  '${appStore.state.value}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          appStore.dispatcher(AppAction.increment);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }

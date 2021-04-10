85333913ea818ecf16bf578c56dd90078b5d12a6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-04-10 03:58:20 -0300

    feat: add redux state & reducer

---
+++ lib/redux/store.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart' hide Action, State;
+import 'package:meta/meta.dart';
+
+class Store<Action, State> extends ChangeNotifier {
+  State _state;
+  final State Function(State state, Action action) reducer;
+
+  Store({
+    @required State initialState,
+    @required this.reducer,
+  }) : _state = initialState;
+
+  State get state => _state;
+
+  void dispatcher(Action action) {
+    _state = reducer(state, action);
+
+    notifyListeners();
+  }
+}
